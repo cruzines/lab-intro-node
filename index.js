11caa7ff37585bc21d7c5fa4def0d847f2b1bc62
Ines Cruz <inesmncruz@gmail.com>
2021-11-09 15:24:19 +0000

    all iterations

--- index.js
+++ index.js
@@ -1,17 +1,58 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((first, second) => {
+      return first - second;
+    });
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (this.items[pos] != undefined) {
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sum = 0;
+    if (!this.items.length) {
+      return 0;
+    } else {
+      this.items.forEach((ele) => (sum += ele));
+    }
+    return sum;
+  }
 
-  avg() {}
+  avg() {
+    if (!this.items.length) {
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum() / this.items.length;
+    }
+  }
 }
 
 module.exports = SortedList;
